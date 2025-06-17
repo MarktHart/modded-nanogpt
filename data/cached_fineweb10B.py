d92d167684a7380fceaec5c148e28ee528e0841d
MarktHart <git@markhart.nl>
2025-06-17 14:33:29 +0200

    Update cached_fineweb10B.py
    Update cached_fineweb10 to output curl

--- data/cached_fineweb10B.py
+++ data/cached_fineweb10B.py
@@ -1,13 +1,10 @@
 import os
 import sys
-from huggingface_hub import hf_hub_download
-# Download the GPT-2 tokens of Fineweb10B from huggingface. This
-# saves about an hour of startup time compared to regenerating them.
+
 def get(fname):
     local_dir = os.path.join(os.path.dirname(__file__), 'fineweb10B')
     if not os.path.exists(os.path.join(local_dir, fname)):
-        hf_hub_download(repo_id="kjj0/fineweb10B-gpt2", filename=fname,
-                        repo_type="dataset", local_dir=local_dir)
+	print(f"curl -o {os.path.join(local_dir, fname)} https://huggingface.co/datasets/kjj0/fineweb10B-gpt2/resolve/main/{fname}")
 get("fineweb_val_%06d.bin" % 0)
 num_chunks = 103 # full fineweb10B. Each chunk is 100M tokens
 if len(sys.argv) >= 2: # we can pass an argument to download less
